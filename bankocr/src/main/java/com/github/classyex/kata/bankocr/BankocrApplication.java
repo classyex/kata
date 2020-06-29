87bf05473e66286f1e2eacd63e82d2814381c33e
邵学友 <shaoxueyou@yetingfm.com>
2020-06-29 12:23:36 +0800

    init

---
+++ bankocr/src/main/java/com/github/classyex/kata/bankocr/BankocrApplication.java
@@ -0,0 +1,13 @@
+package com.github.classyex.kata.bankocr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankocrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankocrApplication.class, args);
+    }
+
+}
---
+++ bankocr/src/test/java/com/github/classyex/kata/bankocr/BankocrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.classyex.kata.bankocr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankocrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
